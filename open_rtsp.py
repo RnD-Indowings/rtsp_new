a1a1e380bae1c21a5976b9dde3e0c72578f3d2d6
Udit Ray <132336609+uditray02@users.noreply.github.com>
2024-12-21 18:12:53 +0530

    Create open_rtsp.py

---
+++ open_rtsp.py
@@ -0,0 +1,15 @@
+import cv2
+
+cv2.namedWindow("RTSP View", cv2.WINDOW_NORMAL)
+cap = cv2.VideoCapture("rtsp://192.168.2.95:8553/video_stream")
+while True:
+    
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow("RTSP View", frame)
+        cv2.waitKey(1)
+    else:
+        print("unable to open stream")
+        break
+cap.release()
+cv2.destroyAllWindows()
