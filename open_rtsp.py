eff3e9657ab4d874cba1f12e4b1ea6fe4f6105e6
Udit Ray <132336609+uditray02@users.noreply.github.com>
2024-12-23 10:37:50 +0530

    open_rtsp.py

--- open_rtsp.py
+++ open_rtsp.py
@@ -1,7 +1,7 @@
 import cv2
 
 cv2.namedWindow("RTSP View", cv2.WINDOW_NORMAL)
-cap = cv2.VideoCapture("rtsp://192.168.2.95:8553/video_stream")
+cap = cv2.VideoCapture("rtsp://192.168.2.95:8553/video_stream")   #iwpl1u rpi5 address in correspondence to the drone's aviator
 while True:
     
     ret, frame = cap.read()
